d5b45fa1c0dc7c3f32e0b8d64bfa87b712430744
lixiaodong <997375155@qq.com>
2019-11-21 14:06:50 +0800

    change

--- saas_bean/src/test/java/testExcel/ExcelTest.java
+++ saas_bean/src/test/java/testExcel/ExcelTest.java
@@ -5,13 +5,6 @@ import org.junit.Test;
 
 public class ExcelTest {
 
-    @Test
-    public void testReadExcel() {
-        try {
-            new ExcelParse().parse("D:\\HEIMA\\学习资料\\saas\\day10\\06 百万数据解析\\测试.xlsx");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+
 
 }
